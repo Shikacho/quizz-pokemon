4ceec083d0c3b20f7e400f8e6515dc6a861de84b
Ryan Bensyour <ryan.bensyour@gmail.com>
2025-02-16 14:59:52 +0100

    integration API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Quiz from "./components/Quiz";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Header"; 
 import "../src/styles/App.css";
 import "../src/styles/footer.css";
 
@@ -10,11 +9,20 @@ function App() {
   return (
     <Router>
       <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Quiz />} />
-        </Routes>
-        <Footer />
+        <Header /> 
+        <Quiz />
+        <footer>
+  <p>
+    Ce projet est un travail dérivé non officiel basé sur Pokémon. Les sprites officiels appartiennent à © Nintendo/Creatures Inc./GAME FREAK Inc.
+  </p>
+  <div className="link-container">
+    <span>Ressources via : </span>
+    <a href="https://github.com/PokeAPI/" target="_blank" rel="noopener noreferrer">
+      PokeAPI
+    </a>
+  </div>
+</footer>
+
       </div>
     </Router>
   );
